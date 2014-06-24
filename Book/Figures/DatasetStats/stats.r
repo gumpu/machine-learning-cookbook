ec6d5417e09f45ff7bc3e33fb8ae3f911118e12a
Frans Slothouber <frans.slothouber@gmail.com>
2014-06-24 09:35:34 +0200

    Multiply Figures in Latex

--- Book/Figures/DatasetStats/stats.r
+++ Book/Figures/DatasetStats/stats.r
@@ -1,10 +1,21 @@
-load("dataset.rdata")
+load("../../../RawData/Owls/dataset.rdata")
 
 # pdf(file="age_table.pdf")
 # plot(table(dataset$age))
 # dev.off()
 
+pdf.options(family = "Bookman", width=4, height=4)
 pdf(file="age_density.pdf")
-plot(density(dataset$age), main="Age density")
+plot(density(dataset$age), xlab="Age", main=NA)
 dev.off()
+
+pdf(file="height_density.pdf")
+plot(density(dataset$height), xlab="Height", main=NA)
+dev.off()
+
+pdf(file="weight_density.pdf")
+plot(density(dataset$weight), xlab="Weight", main=NA)
+dev.off()
+
+
 
